9de6e93ef031b83cc2dadb088e3ef4cb9e79a308
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-01-15 18:53:29 +0900

    18:53

---
+++ src/Chapter03/OverflowExampleDe.java
@@ -0,0 +1,10 @@
+package Chapter03;
+
+public class OverflowExampleDe {
+    public static void main(String[] args) {
+        int x = 1000000;
+        int y = 1000000;
+        int z = x * y;
+        System.out.println(z);
+    }
+}
