91799850dcd92634092b00246c82d67101cfaf4e
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-01-15 22:49:58 +0900

    챕터 3 확인 문제 완료

---
+++ src/Chapter03/Check/Exercise04.java
@@ -0,0 +1,14 @@
+package Chapter03.Check;
+
+public class Exercise04 {
+    public static void main(String[] args) {
+        int pencils = 534;
+        int students = 30;
+
+        int pencilsPerStudent = pencils / students;
+        System.out.println(pencilsPerStudent);
+
+        int pencilsLeft = pencils % students;
+        System.out.println(pencilsLeft);
+    }
+}
---
+++ src/Chapter03/Check/Exercise05.java
@@ -0,0 +1,8 @@
+package Chapter03.Check;
+
+public class Exercise05 {
+    public static void main(String[] args) {
+        int value = 356;
+        System.out.println(value - value % 100);
+    }
+}
