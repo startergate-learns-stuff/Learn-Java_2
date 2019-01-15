6777844ae4e484c3d19ec2765ff3a11ff145647b
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-01-15 19:53:56 +0900

    19:53

--- src/Chapter03/BitReverseOperatorExample.java
+++ src/Chapter03/BitReverseOperatorExample.java
@@ -3,8 +3,8 @@ package Chapter03;
 public class BitReverseOperatorExample {
     public static void main(String[] args) {
         int v1 = 10;
-        int v2 = -v1;
-        int v3 = -v1 + 1;
+        int v2 = ~v1;
+        int v3 = ~v1 + 1;
         System.out.println(toBinaryString(v1) + " 십진수: " + v1 + ")");
         System.out.println(toBinaryString(v2) + " 십진수: " + v2 + ")");
         System.out.println(toBinaryString(v3) + " 십진수: " + v3 + ")");
--- src/Chapter03/CheckOverflowExample.java
+++ src/Chapter03/CheckOverflowExample.java
@@ -9,7 +9,7 @@ public class CheckOverflowExample {
             System.out.println("오버플로우가 발생하여 정확하게 계산할 수 없음");
         }
     }
-    public static int safeAdd(int left, int right) {
+    private static int safeAdd(int left, int right) {
         if ((right>0)) {
             if(left>(Integer.MAX_VALUE - right)) {
                 throw new ArithmeticException("오버플로우 발생");
