e6388f8a6cb62b0643c13f1c0b821568492f3cf5
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-01-16 14:04:46 +0900

    14:04

---
+++ src/Chapter05/ArrayCopyExample.java
@@ -0,0 +1,14 @@
+package Chapter05;
+
+public class ArrayCopyExample {
+    public static void main(String[] args) {
+        String[] oldStrArray = {"Java", "array", "copy"};
+        String[] newStrArray = new String[5];
+
+        System.arraycopy(oldStrArray, 0, newStrArray, 0, oldStrArray.length);
+
+        for (int i = 0; i < newStrArray.length; i++) {
+            System.out.print(newStrArray[i] + ", ");
+        }
+    }
+}
